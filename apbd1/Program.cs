06663254661e0eebf3406c5cb757631b1f38a93b
Kira Elblaus <s31642@pjwstk.edu.pl>
2026-03-15 18:48:09 +0100

    Implemented a simple class with one function

--- apbd1/Program.cs
+++ apbd1/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("asdsadasda!");
+public class apbd1
+{
+    public static void Main()
+    {
+        Console.WriteLine("Second commit");
+    }
+}
